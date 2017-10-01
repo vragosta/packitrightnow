f8bd37e47292da5b182f0b406578db49d16e70d9
Vincent Ragosta <vincentpasqualeragosta@gmail.com>
2017-10-01 11:42:05 -0400

    constructed footer

--- themes/twenty-seventeen/footer.php
+++ themes/twenty-seventeen/footer.php
@@ -12,6 +12,11 @@ namespace PackItRightNow;
 ?>
 
 	<?php get_partial( 'template-parts/content-mobile-menu' ); ?>
+
+	<footer class="footer">
+		<?php get_partial( 'template-parts/content-menu' ); ?>
+		<p>&copy; <?php echo date( 'Y' ); ?> Pack It Right LLC. All Rights Reserved. Responsive Website by <a href="http://vincentragosta.com">Vincent Ragosta</a></p>
+	</footer>
 	</body>
 </html>
 <?php wp_footer(); ?>
