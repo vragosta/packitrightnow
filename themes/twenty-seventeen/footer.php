90fdf25fc485dca1baca7ff639e001a3aa9e1e08
Vincent Ragosta <vragosta@storycorps.org>
2017-10-02 10:28:40 -0400

    added safety zone logo to footer

--- themes/twenty-seventeen/footer.php
+++ themes/twenty-seventeen/footer.php
@@ -11,15 +11,18 @@ namespace PackItRightNow;
 
 ?>
 
-	<?php get_partial( 'template-parts/content-mobile-menu' ); ?>
+		<?php get_partial( 'template-parts/content-mobile-menu' ); ?>
 
-	<footer class="footer">
-		<nav class="menu">
-			<a href="<?php echo home_url( '/about/' ); ?>">About Us</a>
-			<a href="<?php echo home_url( '/contact/' ); ?>">Contact Us</a>
-		</nav>
-		<p>&copy; <?php echo date( 'Y' ); ?> Pack It Right LLC. All Rights Reserved. Responsive Website by <a href="http://vincentragosta.com">Vincent Ragosta</a></p>
-	</footer>
+		<footer class="footer">
+			<nav class="menu">
+				<a href="<?php echo home_url( '/about/' ); ?>">About Us</a>
+				<a href="<?php echo home_url( '/contact/' ); ?>">Contact Us</a>
+			</nav>
+			<p>&copy; <?php echo date( 'Y' ); ?> Pack It Right LLC. All Rights Reserved. Responsive Website by <a href="http://vincentragosta.com">Vincent Ragosta</a></p>
+			<div class="sponsors">
+				<a href="http://safety-zone.com/" target="_blank"><img src="<?php echo PACKITRIGHTNOW_TEMPLATE_URL . '/assets/images/Cutlery_Gilbert_Black.jpg'; ?>" /></a>
+			</div>
+		</footer>
 	</body>
 </html>
 <?php wp_footer(); ?>
