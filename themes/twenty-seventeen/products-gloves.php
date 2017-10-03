6129f0dc8fc192b8f4a0ebf002b4e4611b93dbc4
Vincent Ragosta <vragosta@storycorps.org>
2017-10-03 13:36:37 -0400

    temporary solution to gloves menu

--- themes/twenty-seventeen/products-gloves.php
+++ themes/twenty-seventeen/products-gloves.php
@@ -16,52 +16,37 @@ get_header(); ?>
 			<h2>Gloves</h2>
 			<p>Nullam quis risus eget urna mollis ornare vel eu leo. Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
 		</div>
-		<div class="col-xs-12 col-sm-6">
-
+		<div class="col-xs-12 col-sm-3">
+			<ul>
+				<li><a href="#bladeguard">Blade Guard</a></li>
+				<li><a href="#chemical">Chemical</a></li>
+				<li><a href="#cottoncanvas">Cotton Canvas</a></li>
+				<li><a href="#dipped">Dipped</a></li>
+				<li><a href="#dotted">Dotted</a></li>
+				<li><a href="#drivers">Drivers</a></li>
+				<li><a href="#fingercot">Finger Cot</a></li>
+				<li><a href="#fryer">Fryer</a></li>
+				<li><a href="#heat">Heat</a></li>
+				<li><a href="#hidex">Hidex</a></li>
+			</ul>
+		</div>
+		<div class="col-xs-12 col-sm-3">
+			<ul>
+				<li><a href="#inspector">Inspector</a></li>
+				<li><a href="#insulated">Insulated</a></li>
+				<li><a href="#jersey">Jersey</a></li>
+				<li><a href="#latex">Latex</a></li>
+				<li><a href="#nidex">Nidex</a></li>
+				<li><a href="#nitrile">Nitrile</a></li>
+				<li><a href="#poly">Poly</a></li>
+				<li><a href="#pvc">PVC</a></li>
+				<li><a href="#stringknit">String Knit</a></li>
+				<li><a href="#vinyl">Vinyl</a></li>
+				<li><a href="#welders">Welders</a></li>
+				<li><a href="#work">Work</a></li>
+			</ul>
 		</div>
 	</div>
-	<!-- <div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-sm-3">
-				<ul>
-					<li><a href="#bladeguard">Blade Guard</a></li>
-					<li><a href="#chemical">Chemical</a></li>
-					<li><a href="#cottoncanvas">Cotton Canvas</a></li>
-					<li><a href="#dipped">Dipped</a></li>
-					<li><a href="#dotted">Dotted</a></li>
-				</ul>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<ul>
-					<li><a href="#drivers">Drivers</a></li>
-					<li><a href="#fingercot">Finger Cot</a></li>
-					<li><a href="#fryer">Fryer</a></li>
-					<li><a href="#heat">Heat</a></li>
-					<li><a href="#hidex">Hidex</a></li>
-				</ul>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<ul>
-					<li><a href="#inspector">Inspector</a></li>
-					<li><a href="#insulated">Insulated</a></li>
-					<li><a href="#jersey">Jersey</a></li>
-					<li><a href="#latex">Latex</a></li>
-					<li><a href="#nidex">Nidex</a></li>
-				</ul>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<ul>
-					<li><a href="#nitrile">Nitrile</a></li>
-					<li><a href="#poly">Poly</a></li>
-					<li><a href="#pvc">PVC</a></li>
-					<li><a href="#stringknit">String Knit</a></li>
-					<li><a href="#vinyl">Vinyl</a></li>
-					<li><a href="#welders">Welders</a></li>
-					<li><a href="#work">Work</a></li>
-				</ul>
-			</div>
-		</div>
-	</div> -->
 </div>
 
 <?php get_footer(); ?>
